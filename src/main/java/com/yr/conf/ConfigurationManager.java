107967084cf7a9b4a540d0437278b563fa7ffac8
dengbp <siqing0822@163.com>
2020-06-11 20:11:02 +0800

    更新readme

--- src/main/java/com/yr/conf/ConfigurationManager.java
+++ src/main/java/com/yr/conf/ConfigurationManager.java
@@ -23,7 +23,7 @@ final public class ConfigurationManager {
 	static {
 		try {
 			InputStream in = ConfigurationManager.class
-					.getClassLoader().getResourceAsStream("my.properties");
+					.getClassLoader().getResourceAsStream("big.properties");
 			prop.load(in);
 		} catch (Exception e) {
 			e.printStackTrace();
